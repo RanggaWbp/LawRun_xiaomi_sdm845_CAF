482b8ed29444f5cf1bc10d23b1eff827895403a5
RanggaWbp <RanggaWbp@users.noreply.github.com>
2026-09-25 17:50:14 +0700

    ak3/yml: fix broken installer — restore full AnyKernel3 flash logic (was overwritten wholesale, so zip installed but never wrote the kernel); device check + model names; zip prefix ReSukiSU-susfs_<device>-<DDMMYYYY>; never clobber same-day releases (-r2, -r3...); kernel LOCALVERSION -perf -> -ReBocchi so flashed kernel is identifiable

---
+++ .github/ak3-template/anykernel.sh
@@ -0,0 +1,44 @@
+### AnyKernel3 Ramdisk Mod Script
+## ReBocchi (ReSukiSU v4.2.0-rc3-susfs) by RanggaWbp — Xiaomi SDM845
+
+### AnyKernel setup
+# global properties
+properties() { '
+kernel.string=__AK3_KERNEL_STRING__
+do.devicecheck=1
+do.modules=0
+do.systemless=0
+do.cleanup=1
+do.cleanuponabort=0
+device.name1=__AK3_DEVICE1__
+device.name2=__AK3_DEVICE2__
+device.name3=
+device.name4=
+device.name5=
+supported.versions=
+supported.patchlevels=
+supported.vendorpatchlevels=
+'; } # end properties
+
+
+### AnyKernel install
+## boot files attributes
+boot_attributes() {
+set_perm_recursive 0 0 755 644 $RAMDISK/*;
+set_perm_recursive 0 0 750 750 $RAMDISK/init* $RAMDISK/sbin;
+} # end attributes
+
+# boot shell variables
+BLOCK=auto;
+IS_SLOT_DEVICE=1;
+RAMDISK_COMPRESSION=auto;
+PATCH_VBMETA_FLAG=auto;
+
+# import functions/variables and setup patching - see for reference (DO NOT REMOVE)
+. tools/ak3-core.sh;
+
+# boot install
+dump_boot;
+
+write_boot;
+## end boot install
